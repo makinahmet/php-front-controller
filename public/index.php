6b79064ae32e43eda8d10297c0e0455198876bd9
makinahmet <makinahmet@gmail.com>
2024-03-15 02:55:07 +0300

    update

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
-$URI = $_SERVER['REQUEST_URI'];
-$urlQueries = explode("/", $URI);
+$URI = $_SERVER['REQUEST_URI'];     //main URL
+$urlQueries = explode("/", $URI);   //explode the URL with "/" sign. This returns an array.
+
+/*
+index 0 is empty
+index 1 is base folder
+index 2 is page name
+index 3 is an action
+index 4 is an action
+index 5 is an action etc.
+*/
+
+if($urlQueries[2] == "")
+{
+    include("controllers/HomeController.php");
+}
 ?>
